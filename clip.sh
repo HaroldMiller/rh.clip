bccd3a5377c1cd6b88b19bbb4a1808c1a5de5861
Harold Miller <harold@redhat.com>
2014-08-25 12:12:49 -0700

    clip.sh version 0.01 - not yet working - pre-alpha

--- clip.sh
+++ clip.sh
@@ -1,10 +1,11 @@
-#!/bin/bash -x
-#    clip version 0.00 Written by Harold Miller - harold@redhat.com
+#!/bin/bash 
+#    clip version 0.01 Written by Harold Miller - harold@redhat.com
 #    Removes lines from (log) text files, to allow the interesting stuff to be visible
 
 keyfile="clip.key"
 
 do_help() {
+        echo -e "Clip Version 0.01\n"
         echo -e "Usage: clip [file name]"
         echo -e " or "
         echo -e " clipadd [key term]"
@@ -31,18 +32,21 @@ if [[ `basename $0` == "clip" && -z $1 ]]
 
 case `basename $0` in 
    "clip"      )   orig_filename=$1; 
+                   working_filename=$1;
                    grepkey="";
+                   declare -i j;
                    j=0;
                    while [ 1 ]
                      do
                        read grepkey || break;
                        echo 'grepkey='$grepkey;
                        temp_filename=$orig_filename'.'$j;
-                       grep -v $grepkey $orig_filename > $temp_filename;
-                       orig_filename=$temp_filename; 
-                       j=j+1;
-                     done < $keyfile;
-                   clipped_filename=$temp_filename ;;
+                       echo 'temp_filename='$temp_filename;
+ #  here is my bug. I need the following line to be executed:
+                       grep -v \"$grepkey\" $working_filename > $temp_filename;
+                       working_filename=$temp_filename; 
+                       j=$j+1;
+                     done < $keyfile;;
    "clipadd"    )  echo $1 >> clip.key ;;
    "clipclear"  )  rm clip.key; touch clip.key ;;
 esac
